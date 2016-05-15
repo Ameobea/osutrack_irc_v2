5ee7bd0d02df6ac2c0bbbd49e9cdc3c1cfef0776
Tibo <Tibo442@users.noreply.github.com>
2016-05-15 12:03:00 +0200

    Fixes links
    http://i.imgur.com/UDdv8Q7.png http://i.imgur.com/Z0SrGBd.png
    
    Fixed links: http://i.imgur.com/TtRfOIt.png http://i.imgur.com/33MrDnE.png

--- src/commands.js
+++ src/commands.js
@@ -44,11 +44,11 @@ commands.parseCommand = (nick, message, client)=>{
     }else if(command == "!contact"){
       f("PM me on the osu! website, send an email to me@ameo.link, or PM me on reddit /u/ameobea.");
     }else if(command == "!reddit"){
-      f("[https://reddit.com/r/osugame](/r/osugame)");
+      f("[https://reddit.com/r/osugame /r/osugame]");
     }else if(command == "!site"){
-      f("[https://ameobea.me/osutrack/](osu!track website)");
+      f("[https://ameobea.me/osutrack/ osu!track website]");
     }else if(command == "!forums"){
-      f("[https://osu.ppy.sh/forum/](osu! forums)");
+      f("[https://osu.ppy.sh/forum/ osu! forums]");
     }else{
       if(message.length > 0 && message[0] == "!"){
         f("Unknown command; try !help");
@@ -71,7 +71,7 @@ commands.update = (nick, split)=>{
       if(data.mode !== 0 && data.mode !== "0"){
         res += `/${modeIdToString(data.mode)}`;
       }
-      res += "](osu!track).";
+      res += " osu!track].";
 
       if(data.levelup !== false && data.levelup != "false"){
         return [res, `Congratulations on leveling up!`];
@@ -209,28 +209,28 @@ commands.givePP = ()=>{
   var lucky = Math.round(Math.random() * 11);
 
   if(lucky === 0){
-    return "[http){//osu.ppy.sh/b/219311 Muryoku P - Existence [appearance AR9]]: 958pp | 95: 952pp | 98: 965pp | 99: 998pp | 100: 1002pp | 2:30 ★ 8.32 ♫ 290 AR9";
+    return "[http://osu.ppy.sh/b/219311 Muryoku P - Existence [appearance AR9]]: 958pp | 95: 952pp | 98: 965pp | 99: 998pp | 100: 1002pp | 2:30 ★ 8.32 ♫ 290 AR9";
   }else if(lucky == 1){
-    return "[http){//osu.ppy.sh/b/655679 Omoi - Snow Drive [Rabbit's Jumping Style]]: 782pp | 95: 775pp | 98: 783pp | 99: 792pp | 100: 799pp | 3:59 ★ 8.24 ♫ 224 AR9.7";
+    return "[http://osu.ppy.sh/b/655679 Omoi - Snow Drive [Rabbit's Jumping Style]]: 782pp | 95: 775pp | 98: 783pp | 99: 792pp | 100: 799pp | 3:59 ★ 8.24 ♫ 224 AR9.7";
   }else if(lucky == 2){
-    return "[http){//osu.ppy.sh/b/553082 ZOGRAPHOS (Yu Asahina+Yamajet) - Verse IV [DoKo]]: 755pp | 95: 745pp | 98: 759pp | 99: 768pp | 100: 782pp | 1:59 ★ 8.05 ♫ 212 AR9.6";
+    return "[http://osu.ppy.sh/b/553082 ZOGRAPHOS (Yu Asahina+Yamajet) - Verse IV [DoKo]]: 755pp | 95: 745pp | 98: 759pp | 99: 768pp | 100: 782pp | 1:59 ★ 8.05 ♫ 212 AR9.6";
   }else if(lucky == 3){
-    return "[http){//osu.ppy.sh/b/529432 Kagamine Rin & Len - Transformation [DoKo]]: 722pp | 95: 695pp | 98: 709pp | 99: 723pp | 100: 733pp | 2:27 ★ 7.96 ♫ 248 AR10";
+    return "[http://osu.ppy.sh/b/529432 Kagamine Rin & Len - Transformation [DoKo]]: 722pp | 95: 695pp | 98: 709pp | 99: 723pp | 100: 733pp | 2:27 ★ 7.96 ♫ 248 AR10";
   }else if(lucky == 4){
-    return "[http){//osu.ppy.sh/b/460501 Various Artists - osu! ~ Death Stream Compilation 3 [Marathon]]: 778pp | 95: 755pp | 98: 768pp | 99: 779pp | 100: 792pp | 5:42 ★ 7.95 ♫ 185 AR10";
+    return "[http://osu.ppy.sh/b/460501 Various Artists - osu! ~ Death Stream Compilation 3 [Marathon]]: 778pp | 95: 755pp | 98: 768pp | 99: 779pp | 100: 792pp | 5:42 ★ 7.95 ♫ 185 AR10";
   }else if(lucky == 5){
-    return "[http){//osu.ppy.sh/b/465543 The Quick Brown Fox - Shut Down Everything [490 Style]]: 705pp | 95: 689pp | 98: 700pp | 99: 708pp | 100: 715pp | 2:04 ★ 7.89 ♫ 220 AR10";
+    return "[http://osu.ppy.sh/b/465543 The Quick Brown Fox - Shut Down Everything [490 Style]]: 705pp | 95: 689pp | 98: 700pp | 99: 708pp | 100: 715pp | 2:04 ★ 7.89 ♫ 220 AR10";
   }else if(lucky == 6){
-    return "[http){//osu.ppy.sh/b/129891 xi - FREEDOM DiVE [FOUR DIMENSIONS]]: -15pp | 95: 463pp | 98: 497pp | 99: 516pp | 100: 540pp | 4:17 ★ 7.07 ♫ 222.22 AR9";
+    return "[http://osu.ppy.sh/b/129891 xi - FREEDOM DiVE [FOUR DIMENSIONS]]: -15pp | 95: 463pp | 98: 497pp | 99: 516pp | 100: 540pp | 4:17 ★ 7.07 ♫ 222.22 AR9";
   }else if(lucky == 7){
-    return "[http){//osu.ppy.sh/b/131891 The Quick Brown Fox - The Big Black [WHO'S AFRAID OF THE BIG BLACK]]: 0pp | 95: 283pp | 98: 298pp | 99: 308pp | 100: 322pp | 2:18 ★ 6.58 ♫ 360.3 AR10";
+    return "[http://osu.ppy.sh/b/131891 The Quick Brown Fox - The Big Black [WHO'S AFRAID OF THE BIG BLACK]]: 0pp | 95: 283pp | 98: 298pp | 99: 308pp | 100: 322pp | 2:18 ★ 6.58 ♫ 360.3 AR10";
   }else if(lucky == 8){
-    return "[http){//osu.ppy.sh/b/236344 Zedd - Clarity feat. Foxes (IOSYS uno DENPA Remix) [Insane]]: 172pp | 95: 129pp | 98: 148pp | 99: 162pp | 100: 183pp | 3:32 ★ 5.02 ♫ 200 AR9";
+    return "[http://osu.ppy.sh/b/236344 Zedd - Clarity feat. Foxes (IOSYS uno DENPA Remix) [Insane]]: 172pp | 95: 129pp | 98: 148pp | 99: 162pp | 100: 183pp | 3:32 ★ 5.02 ♫ 200 AR9";
   }else if(lucky == 9){
-    return "[http){//osu.ppy.sh/b/62297 Tachibana Miya - Miya to Tengoku to Jigoku [Baka]]: 420pp | 95: 1pp | 98: 2pp | 99: 2pp | 100: 4pp | 1:39 ★ 1.11 ♫ 195 AR2";
+    return "[http://osu.ppy.sh/b/62297 Tachibana Miya - Miya to Tengoku to Jigoku [Baka]]: 420pp | 95: 1pp | 98: 2pp | 99: 2pp | 100: 4pp | 1:39 ★ 1.11 ♫ 195 AR2";
   }else if(lucky == 10){
-    return "[https){//osu.ppy.sh/b/735272 TATOE TATOE - TATOE TATOE TATOE [TATOE]]: TATOE | 95: TATOE | 98: TATOE | 99: TATOE | 100: TATOE | TATOE ★ TATOE ♫ TATOE AR TATOE";
+    return "[https://osu.ppy.sh/b/735272 TATOE TATOE - TATOE TATOE TATOE [TATOE]]: TATOE | 95: TATOE | 98: TATOE | 99: TATOE | 100: TATOE | TATOE ★ TATOE ♫ TATOE AR TATOE";
   }else if(lucky == 11){
-    return "[https){//osu.ppy.sh/b/372245 Knife Party - Centipede [This isn't a map, just a simple visualization]]: 1337pp | 95: 5548 | 98: 5785 | 99: 5998 | 100: 6001 | 45.69 ★ 2:16 ♫ 560 AR10";
+    return "[https://osu.ppy.sh/b/372245 Knife Party - Centipede [This isn't a map, just a simple visualization]]: 1337pp | 95: 5548 | 98: 5785 | 99: 5998 | 100: 6001 | 45.69 ★ 2:16 ♫ 560 AR10";
   }
 };
