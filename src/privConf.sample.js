b597591f26a03e5d9052f2a944983f556ab9ff98
Casey Primozic <me@ameo.link>
2016-05-15 02:41:11 -0400

    Added in sample config file

---
+++ src/privConf.sample.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+Private config.  Contains stuff like passwords, ips, and other classified stuff.
+*/
+var privConfig = exports;
+
+privConfig.ircPassword = 'secret';
+
+privConfig.sqlIp = "localhost";
+privConfig.sqlUsername = "osutrack";
+privConfig.sqlPassword = "password";
+privConfig.sqlUserDb = "osu_track";
+privConfig.sqlUserTable = "online_users";
